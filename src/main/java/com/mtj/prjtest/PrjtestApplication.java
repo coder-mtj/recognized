4c6df9b9ff2bdb6680531d712c9a0beb57d674ee
MTJ <mtj295152@gmail.com>
2024-01-03 14:54:50 +0800

    添加yaml配置

---
+++ src/main/java/com/mtj/prjtest/PrjtestApplication.java
@@ -0,0 +1,13 @@
+package com.mtj.prjtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtj/prjtest/PrjtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtj.prjtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
